24c33c428100b61302f9ed9d26aa94b964cf698b
hej hej <jahicanes@hotmail.com>
2020-12-10 14:50:51 +0100

    .

--- EHandelV2/ShoppingCart/Cart.cs
+++ EHandelV2/ShoppingCart/Cart.cs
@@ -50,6 +50,75 @@ namespace EHandelV2.ShoppingCart
             }
 
             Console.WriteLine("Total Price: " + totalPrice.ToString());
+            Console.WriteLine("Vill du fortsätta handla skriv 1?");
+            Console.WriteLine("Vill du gå till betalning skriv 2");
+            if (Products.Count > 0)
+            {
+                Console.WriteLine("Rensa produkt från varukorg skriv 3");
+                Console.WriteLine("Rensa varukorg skriv 4");
+            }
+            string input = Console.ReadLine();
+
+            if (input == "1") 
+            {
+                return;
+            }
+            else if (input == "2") 
+            {
+                betalning();
+                Console.WriteLine();
+                return;
+            }
+            else if (input == "3") 
+            {
+                Console.WriteLine("Välj produkten som du vill ta bort");
+                int input2 = Convert.ToInt32(Console.ReadLine());
+                RemoveProduct(input2);
+                Console.WriteLine("Produkten är borttagen");
+                
+            }
+            else if(input == "4") 
+            {
+                Products.Clear();
+            
+            }
+            ShowCart();
+
+
+
+        }
+
+        public void betalning() 
+        {
+            Console.WriteLine("Skriv ditt namn:");
+            name = Console.ReadLine();
+            Console.WriteLine("Skriv din adress:");
+            adress = Console.ReadLine();
+            Console.Clear();
+
+            int totalPrice = 0;
+
+            foreach (Product ProductCart in this.Products.Values)
+            {
+                Console.WriteLine("Name: " + ProductCart.Name);
+                Console.WriteLine("ID: " + ProductCart.ID);
+                Console.WriteLine("Price: " + ProductCart.Price);
+                Console.WriteLine();
+
+                totalPrice += ProductCart.Price;
+            }
+            Products.Clear();
+            Console.WriteLine("Total Price: " + totalPrice.ToString());
+            Console.WriteLine();
+            ShowReceipt();
+
+
+
+
+
+
+
+
         }
 
         public void AddCustomerInfo()
@@ -58,16 +127,29 @@ namespace EHandelV2.ShoppingCart
             name = Console.ReadLine();
             Console.WriteLine("Skriv din adress:");
             adress = Console.ReadLine();
+            Console.Clear();
+            int totalPrice = 0;
+
+            foreach (Product ProductCart in this.Products.Values)
+            {
+                Console.WriteLine("Name: " + ProductCart.Name);
+                Console.WriteLine("ID: " + ProductCart.ID);
+                Console.WriteLine("Price: " + ProductCart.Price);
+                Console.WriteLine();
+
+                totalPrice += ProductCart.Price;
+            }
+            Products.Clear();
+            Console.WriteLine("Total Price: " + totalPrice.ToString());
+            ShowReceipt();
 
         }
 
         public void ShowReceipt()
         {
-            ShowCart();
-            
-            Console.WriteLine(name);
-            Console.WriteLine(adress);
-
+            Console.WriteLine("Tack för din beställning " + name);
+            Console.WriteLine("Din order levereras till " + adress);
+            Console.WriteLine("Vällkommen åter");
         }
     }
 }
